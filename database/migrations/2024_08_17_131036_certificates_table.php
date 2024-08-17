f2aaef68118ba886b8cbc96b5ffd199b28acf6a3
Victor Oseji <vctroseji@gmail.com>
2024-08-17 21:41:36 +0100

    updated commit

---
+++ database/migrations/2024_08_17_131036_certificates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->id();
+            $table->string('certificate_number')->unique()->nullable();
+            $table->string('client_id')->nullable();
+            $table->string('course_id')->unique()->nullable();
+            $table->string('file_path')->unique()->nullable();
+            $table->unsignedBigInteger('uploaded_by')->nullable();
+            $table->foreign('uploaded_by')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
