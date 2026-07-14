93b711823447ebb92a14655ab2dfacf249c922d3
YVictor Oseji <victoroseji@gmail.com>
2026-07-14 19:56:42 +0100

    chore: update backend

--- config/cors.php
+++ config/cors.php
@@ -1,25 +1,47 @@
 <?php
 
-return [
+// return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Cross-Origin Resource Sharing (CORS) Configuration
+//     |--------------------------------------------------------------------------
+//     |
+//     | Here you may configure your settings for cross-origin resource sharing
+//     | or "CORS". This determines what cross-origin operations may execute
+//     | in web browsers. You are free to adjust these settings as needed.
+//     |
+//     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+//     |
+//     */
+
+//     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+//     'allowed_methods' => ['*'],
+
+//     'allowed_origins' => ['*'],
+
+//     'allowed_origins_patterns' => [],
+
+//     'allowed_headers' => ['*'],
+
+//     'exposed_headers' => [],
+
+//     'max_age' => 0,
+
+//     'supports_credentials' => false,
+
+// ];
+
+return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://app.ilearn360africa.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -30,5 +52,4 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,
-
 ];
